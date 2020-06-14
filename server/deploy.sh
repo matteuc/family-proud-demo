7e918959f022891c01f6648cd07486e0aa0bff53
Matt <ymc006@ucsd.edu>
2020-06-14 12:19:00 -0700

    added one deployment file to create cluster and create other services

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,11 +1,11 @@
 # Build the API server image
-docker build -t gcr.io/family-proud-intern-demo/api $PWD
+docker build --no-cache -t gcr.io/family-proud-intern-demo/api $PWD
 
 # Push the created image to the GCR
 gcloud docker -- push gcr.io/family-proud-intern-demo/api
 
 # Create a config to hold environmental variables, specifically the URI for MongoDB
-kubectl create configmap api-config --from-literal=MONGODB_URL=$1
+kubectl create configmap api-config --from-literal=MONGODB_URL=mongodb://mongo-0-wrpjr/familyProud
 
 # Create the service to expose the API endpoint
 kubectl apply -f service.yaml
