bc2ba3b31254c14008dd51361752325679ce997a
Matt <ymc006@ucsd.edu>
2020-06-14 13:34:34 -0700

    changed mongo service name and attached service to run on mongo deployment pods

--- server/deploy.sh
+++ server/deploy.sh
@@ -10,7 +10,7 @@ docker build --no-cache -t gcr.io/family-proud-intern-demo/api $dir_path
 gcloud docker -- push gcr.io/family-proud-intern-demo/api
 
 # Create a config to hold environmental variables, specifically the URI for MongoDB
-kubectl create configmap api-config --from-literal=MONGODB_URL=mongodb://mongo-0-wrpjr/familyProud
+kubectl create configmap api-config --from-literal=MONGODB_URL=mongodb://mongo-service/familyProud
 
 # Create the service to expose the API endpoint
 kubectl apply -f $dir_path/service.yaml
