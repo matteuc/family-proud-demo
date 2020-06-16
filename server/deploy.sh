7912ddf125391f6971dce74651d790021e9e62ec
Matt <ymc006@ucsd.edu>
2020-06-15 21:48:06 -0700

    added cleanup scripts

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,13 +1,16 @@
+# This shell script should be run with one argument, your project's ID
+# Ex. 'bash ./deploy.sh [project-name]'
+
 # Retrieve file's directory path
 full_path=$(realpath $0)
  
 dir_path=$(dirname $full_path)
 
 # Build the API server image
-docker build --no-cache -t gcr.io/family-proud-demo/api $dir_path
+docker build --no-cache -t gcr.io/$1/api $dir_path
 
 # Push the created image to the GCR
-gcloud docker -- push gcr.io/family-proud-demo/api
+gcloud docker -- push gcr.io/$1/api
 
 # Create a config to hold environmental variables, specifically the URI for MongoDB
 kubectl create configmap api-config --from-literal=MONGODB_URL=mongodb://mongo-service/familyProud?replicaSet=rs0
