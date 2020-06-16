38f093d5d527b054c957365d0b3a26ec4a6a5e58
Matt <ymc006@ucsd.edu>
2020-06-16 00:26:20 -0700

    deployment images replaced with dynamic CLI arguments

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,3 +1,6 @@
+# This shell script should be run with one argument, your project's ID
+# Ex. 'bash ./deploy.sh [project-name]'
+
 # Retrieve file's directory path
 full_path=$(realpath $0)
  
@@ -9,10 +12,13 @@ IP=$(kubectl get services api-service -o json | jq -r '.status.loadBalancer.ingr
 API_URL="http://${IP}:${PORT}"
 
 # Build the API server image
-docker build --build-arg API_URL=${API_URL} --no-cache -t gcr.io/family-proud-demo/web $dir_path
+docker build --build-arg API_URL=${API_URL} --no-cache -t gcr.io/$1/web $dir_path
 
 # Push the created image to the GCR
-gcloud docker -- push gcr.io/family-proud-demo/web
+gcloud docker -- push gcr.io/$1/web
+
+# Replace Dockerfile with the correct image from your project
+sed -i "s/PROJECT_ID_PLACEHOLDER/$1/g" $dir_path/deployment.yaml
 
 # Create the service to expose the API endpoint
 kubectl apply -f $dir_path/service.yaml
--- server/deploy.sh
+++ server/deploy.sh
@@ -6,6 +6,7 @@ full_path=$(realpath $0)
  
 dir_path=$(dirname $full_path)
 
+
 # Build the API server image
 docker build --no-cache -t gcr.io/$1/api $dir_path
 
@@ -14,6 +15,9 @@ gcloud docker -- push gcr.io/$1/api
 
 # Create a config to hold environmental variables, specifically the URI for MongoDB
 kubectl create configmap api-config --from-literal=MONGODB_URL=mongodb://mongo-service/familyProud?replicaSet=rs0
+
+# Replace Dockerfile with the correct image from your project
+sed -i "s/PROJECT_ID_PLACEHOLDER/$1/g" $dir_path/deployment.yaml
 
 # Create the service to expose the API endpoint
 kubectl apply -f $dir_path/service.yaml
